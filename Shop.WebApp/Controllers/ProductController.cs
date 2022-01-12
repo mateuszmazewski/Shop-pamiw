5a782bcb3f55d048856ad06136b79fd419aaf96b
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2022-01-12 22:26:24 +0100

    Add Product controller and views in WebApp

---
+++ Shop.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Shop.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zawodnicy.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IConfiguration Configuration;
+
+        public ProductController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ContentResult GetHostUrl()
+        {
+            var result = Configuration["RestApiUrl:HostUrl"];
+            return Content(result);
+        }
+
+        private string ControllerName()
+        {
+            string cn = ControllerContext.RouteData.Values["controller"].ToString();
+            return cn;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+            List<ProductVM> productsList = new List<ProductVM>();
+
+            using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+            {
+                using (var response = await httpClient.GetAsync(_restpath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    productsList = JsonConvert.DeserializeObject<List<ProductVM>>(apiResponse);
+                }
+            }
+
+            return View(productsList);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+            ProductVM product = new ProductVM();
+
+            using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<ProductVM>(apiResponse);
+                }
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductVM vm)
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+            ProductVM result;
+
+            try
+            {
+                using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(vm);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    using (var response = await httpClient.PutAsync($"{_restpath}/{vm.Id}", content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        result = JsonConvert.DeserializeObject<ProductVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            return View(new ProductVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductVM vm)
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+            ProductVM result;
+
+            try
+            {
+                using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(vm);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    using (var response = await httpClient.PostAsync(_restpath, content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        result = JsonConvert.DeserializeObject<ProductVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(ProductVM vm)
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+            ProductVM product;
+
+            using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{vm.Id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<ProductVM>(apiResponse);
+                }
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+
+            try
+            {
+                using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+                {
+                    using (var response = await httpClient.DeleteAsync($"{_restpath}/{id}"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            string _restpath = GetHostUrl().Content + ControllerName();
+            ProductVM product;
+
+            using (var httpClient = new HttpClient(new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } }))
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<ProductVM>(apiResponse);
+                }
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ Shop.WebApp/Models/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Core.Domain;
+
+namespace Shop.WebApp.Models
+{
+    public class ProductVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Stock { get; set; }
+        public UnitOfMeasurement UnitOfMeasurement { get; set; }
+    }
+}
